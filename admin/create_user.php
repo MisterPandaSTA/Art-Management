58a2b9b012d66c0d3a6f6392bf8a122423ead6bd
Mister Panda <Mister Panda>
2017-02-12 19:32:58 +0100

    j'ajoute tout les fichiers

--- admin/create_user.php
+++ admin/create_user.php
@@ -2,4 +2,9 @@
 
 require_once('include/includes.php');
 
-new
+$new_user = new user()
+$new_user->formcreate('create_user.php')
+
+?>
+
+<form><input type="password" name="name"></form>
---
+++ index.php
@@ -0,0 +1,21 @@
+
+
+<!DOCTYPE html><html>
+<meta charset="utf-8">
+<head>
+</head>
+<body>
+<form action="" method="POST">
+<label for="nom">Nom :</label>
+<input type="text" name="nom">
+<label for="prenom">Prenom :</label>
+<input type="text" name="prenom">
+<label for="email">Email:</label>
+<input type="text" name="email">
+<label for="password">Mot de passe :</label>
+<input type="password" name="password">
+</form>
+
+</body>
+<?php
+?>
