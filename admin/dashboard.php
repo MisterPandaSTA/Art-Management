1b6c0c7c9d7bbce30fd2af03442aa59e8e75a98c
Mister Panda <Mister Panda>
2017-02-20 16:10:12 +0100

    update dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -2,23 +2,30 @@
 
 require_once('include/includes.php');
 
-if(isset($_SESSION['id']) && $_SESSION['permission'] == 'admin'){
-	echo 'ici c\'est pour les admins !';
+if(isset($_SESSION['id'])){
+	if($_SESSION['permission'] == 'utilisateur' || $_SESSION['permission'] == 'admin' ){
+		echo 'ici cest pour les user normaux !';
+	}
+	else($_SESSION['permission'] == 'admin' ){
+		echo 'ya que les admins qui voies ça !';
+	}	
 }
-else(isset($_SESSION['id']) && $_SESSION['permission'] == 'utilisateur'){
-	echo 'ici c\'est pour les admins !';
+else { 
+	echo 'il faut te connecter gros !';
 }
 
 
 ?>
+
 <!DOCTYPE html><html>
 <meta charset="utf-8">
 <head>
+
 </head>
-<body>
+	<body>
+	<p>ici c'est le dashboard</p>
 
-<p>ici c'est le dashboard</p>
-
-</body>
+	</body>
+</html>
 <?php
 ?>
