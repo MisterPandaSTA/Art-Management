60e7b86cc15ec2dd5b91194721090f43751648ae
Mister Panda <Mister Panda>
2017-03-07 16:20:15 +0100

    prépa de script.js avec découpage

--- admin/javascript/script.js
+++ admin/javascript/script.js
@@ -1,9 +1,49 @@
+/*------------------------ 
+    partout
+-------------------------*/
+
+function RedirectLogin(){
+        document.location.href="http://localhost/git/art_management/admin/index.php";
+      }
+
+/*------------------------ 
+    index.php (login)
+-------------------------*/      
+
+
+
 $(document).ready(function () {
 	$('#mdpoublie').on('click', function(){
 		alert("Veuillez contacter votre administrateur pour qu'il réinitialise votre mot de passe.");
 	});
 });
 
-function RedirectLogin(){
-        document.location.href="http://localhost/git/art_management/admin/index.php";
-      }
+
+/*------------------------ 
+  user.php (modif user)
+-------------------------*/
+
+
+/*------------------------ 
+  gestion_user.php (gestion Admin)
+-------------------------*/
+
+
+/*------------------------ 
+  oeuvre.php (gestion des oeuvres)
+-------------------------*/
+
+
+/*------------------------ 
+  exposition.php (gestion des expos)
+-------------------------*/
+
+
+/*------------------------ 
+  artistes.php (gestion des artistes)
+-------------------------*/
+
+
+/*------------------------ 
+    .php ()
+-------------------------*/
